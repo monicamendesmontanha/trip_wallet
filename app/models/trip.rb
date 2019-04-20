5002628ef9f246093ee2be66e7e2613b0f305225
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-20 20:17:47 +1000

    Validates start and end date on the form

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,17 @@ class Trip < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :budget, numericality: true
+
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+ end
 end
