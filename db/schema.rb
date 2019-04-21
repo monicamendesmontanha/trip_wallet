d5ef0fbb7ad897dc5df817028cee08e3c34c640b
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-22 08:54:39 +1000

    update db: User migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_021407) do
+ActiveRecord::Schema.define(version: 2019_04_21_224736) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2019_04_19_021407) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["destination_id"], name: "index_trips_on_destination_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
