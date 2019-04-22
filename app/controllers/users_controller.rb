30e794a10e9ca4ae7b30a6f0b1f2473cfb0e968a
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-22 19:47:06 +1000

    Login upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id
+      log_in @user
       flash[:success] = "Welcome, #{@user.name}!"
       redirect_to @user
     else
