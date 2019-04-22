8ab72b585efb9f7d43dd3fe3210add443b13e79d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-22 10:46:08 +1000

    Make a basic User model,including secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
+
+
+# Breaking down the valid email regex
+# EXPRESSION                                             	MEANING
+#____________________________________________________________________________________________
+# /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                  full regex
+# /	                                                  start of regex
+# \A	                                             match start of a string
+#[\w+\-.]+	                             at least one word character, plus, hyphen, or dot
+#@                                                    literal “at sign”
+#[a-z\d\-.]+                               	at least one letter, digit, hyphen, or dot
+#\.	                                                     literal dot
+#[a-z]+	                                              at least one letter
+#\z	                                                 match end of a string
+#/                                                       	end of regex
+#i                                                   	case-insensitive
+#____________________________________________________________________________________________
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_224736) do
+ActiveRecord::Schema.define(version: 2019_04_22_003554) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2019_04_21_224736) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
